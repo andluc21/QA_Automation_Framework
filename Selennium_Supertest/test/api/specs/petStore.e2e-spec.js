69d2da3cc6b1d2a7956b87e52d7fc1739c3ec412
la250175 <la250175@ncr.com>
2022-12-10 13:24:22 -0800

    update chromedriver

--- Selennium_Supertest/test/api/specs/petStore.e2e-spec.js
+++ Selennium_Supertest/test/api/specs/petStore.e2e-spec.js
@@ -1,4 +1,5 @@
 const { getRequest, postRequest } = require("../services.js/httpUtil");
+const orderId = 11
 
 describe("Pet Store Inventory", () => {
   it("GET/ Retrieving pets by a valid status is successful", async () => {
@@ -6,17 +7,22 @@ describe("Pet Store Inventory", () => {
     expect(getResponse.status).toEqual(200);
   });
 
-  it("POST/ Creating a valid order is successful", async () => {
+  it("POST/ Creating a valid pet order is successful", async () => {
     const payload = {
-        id: 10,
+        id: orderId,
         petId: 20,
-        quantity: 30,
+        quantity: 31,
         shipDate: "2022-10-20T04:11:21.054Z",
         status: "placed",
         complete: true,
       }
 
     const getResponse = await postRequest("/store/order", payload);
+    expect(getResponse.status).toEqual(200);
+  });
+
+  it("GET/ Retrieving the newly created order by orderId is successful", async () => {
+    const getResponse = await getRequest(`/store/order/${orderId}`);
     expect(getResponse.status).toEqual(200);
     console.log(getResponse.body)
   });
