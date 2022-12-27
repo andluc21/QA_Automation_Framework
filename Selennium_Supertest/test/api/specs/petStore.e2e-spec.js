1496345fe1e4a049ce45b83fb19d6c8562a6a62a
Costello <costello@Costellos-MacBook-Pro.local>
2022-12-26 22:04:24 -0800

    test user instead

--- Selennium_Supertest/test/api/specs/petStore.e2e-spec.js
+++ Selennium_Supertest/test/api/specs/petStore.e2e-spec.js
@@ -1,29 +1,49 @@
-const { getRequest, postRequest } = require("../services.js/httpUtil");
-const orderId = 11
+const { getRequest, postRequest, putRequest } = require("../services.js/httpUtil");
+const username = 'madeUpUserName100'
 
 describe("Pet Store Inventory", () => {
   it("GET/ Retrieving pets by a valid status is successful", async () => {
-    const getResponse = await getRequest("/pet/findByStatus?status=sold");
+    const getResponse = await getRequest('/pet/findByStatus?status=sold');
     expect(getResponse.status).toEqual(200);
   });
 
-  it("POST/ Creating a valid pet order is successful", async () => {
+  it("POST/ Creating a valid user is successful", async () => {
     const payload = {
-        id: orderId,
-        petId: 20,
-        quantity: 31,
-        shipDate: "2022-10-20T04:11:21.054Z",
-        status: "placed",
-        complete: true,
-      }
+      id: 50010,          
+      username,
+      firstName: "string",
+      lastName: "string",
+      email: "string",
+      password: "string",
+      phone: "string",
+      userStatus: 0,
+    };
 
-    const getResponse = await postRequest("/store/order", payload);
-    expect(getResponse.status).toEqual(200);
+    const postResponse = await postRequest('/user', payload);
+    expect(postResponse.status).toEqual(200);
+    console.log(postResponse.body);
   });
 
-  it("GET/ Retrieving the newly created order by orderId is successful", async () => {
-    const getResponse = await getRequest(`/store/order/${orderId}`);
+  it("PUT/ Updating a specific user is successful", async () => {
+    const payload = {
+      id: 50010,      
+      username,
+      firstName: "updatedFirstName",
+      lastName: "string",
+      email: "string",
+      password: "string",
+      phone: "string",
+      userStatus: 0,
+    };
+
+    const putResponse = await putRequest(`/user/${username}`, payload); 
+    expect(putResponse.status).toEqual(200);
+    console.log(putResponse.body);
+  });
+
+  it("GET/ Retrieving the newly created user by name is successful", async () => {
+    const getResponse = await getRequest(`/user/${username}`);
     expect(getResponse.status).toEqual(200);
-    console.log(getResponse.body)
+    console.log(getResponse.body);
   });
 });
