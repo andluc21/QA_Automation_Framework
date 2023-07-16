61322cfabb0b5fdbe8c1f565ec57e8b290a5ce2b
Costello <costello@Costellos-MacBook-Pro.local>
2023-07-16 14:36:23 -0700

    fix tests

--- Selennium_Supertest/test/ui/specs/searchResults.spec-e2e.js
+++ Selennium_Supertest/test/ui/specs/searchResults.spec-e2e.js
@@ -9,6 +9,19 @@ describe('Searching for Results on Dell', function() {
     driver = new Builder().forBrowser('chrome').build();
   });
 
+  it('Searching for the term "inspiron 16 laptop" returns results', async function searchValidResult() {
+    await driver.get(`${requestUrl}`);                   
+    await driver.findElement(By.id('mh-search-input')).sendKeys('inspiron 16 laptop');  
+    let searchButton = await driver.findElement(By.className('mh-search-btn mh-search-submit'));
+    await searchButton.click();
+
+    let resultMessage = await driver.findElement(By.css('.dds__col--lg-6.search-pageinfo-smalldevices .pageinfo'));
+    let resultValue = await resultMessage.getAttribute('innerText');
+
+    // Assert the expected result value
+    expect(resultValue).toContain("Showing 1 - 12 of 149 results for 'inspiron 16 laptop'");
+  });
+
   it('Searching for the term "webdriver" returns no results', async function searchNoResult() {
     await driver.get(`${requestUrl}`);                   
     await driver.findElement(By.id('mh-search-input')).sendKeys('webdriver');  
@@ -17,18 +30,11 @@ describe('Searching for Results on Dell', function() {
 
     let resultMessage = await driver.findElement(By.xpath('//*[@class="zero-results-extended-general-frame-text"]'));
     let resultValue = await resultMessage.getText();
+    
+    // Assert the expected result value
     expect(resultValue).toEqual('Sorry, there are no results for "webdriver"');
-  });
 
-  it('Searching for the term "inspiron 16 laptop" returns results', async function searchValidResult() {
-    await driver.get(`${requestUrl}`);                   
-    await driver.findElement(By.id('mh-search-input')).sendKeys('inspiron 16 laptop');  
-    let searchButton = await driver.findElement(By.className('mh-search-btn mh-search-submit'));
-    await searchButton.click();
-
-    let resultMessage = await driver.findElement(By.className('bc-category-label icon-home'));
-    let resultValue = await resultMessage.getText();
-    expect(resultValue).toEqual('Showing results for inspiron 16 laptopin');
+    // await new Promise((resolve) => setTimeout(resolve, 1100500)); // Pause 
   });
 
   afterEach(() => {
